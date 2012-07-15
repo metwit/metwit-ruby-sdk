6aaf26417cd9138e1586df155ef1a26c5fd7f908
Simone D'Amico <sim@me.com>
2012-07-15 16:50:54 +0200

    added documentation to weather attribute

--- lib/metwit/metag.rb
+++ lib/metwit/metag.rb
@@ -1,7 +1,10 @@
 module Metwit
   class Metag
     # @!attribute [rw] weather
-    #   @return {Symbol => Symbol}
+    #   @return Hash
+    #   Is always required in a valid Metag.
+    #   Weather is an Hash with two keys: :status and :details
+    #   Valid :status values are: :sunny, :rainy, :stormy, :snowy, :partly_cloudy, :cloudy, :hailing, :heavy_seas, :calm_seas, :foggy, :snow_flurries, :windy, :clear_moon, :parly_cloudy
     attr_accessor :weather
   end
 end
