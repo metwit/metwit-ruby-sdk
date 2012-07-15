f13560b7d3d77e19c3d75a417eca70010330a1f8
Simone D'Amico <sim@me.com>
2012-07-15 18:07:03 +0200

    metag weather status must not be nil

--- lib/metwit/metag.rb
+++ lib/metwit/metag.rb
@@ -1,11 +1,25 @@
 module Metwit
+  # Metags are the weather tags
   class Metag
     # @!attribute [rw] weather
     #   @return Hash
     #   Is always required in a valid Metag.  
-    #   Weather is an Hash with two keys: :status and :details
+    #   Weather is an Hash with two keys: :status and :details  
     #   Valid :status values are:  
     #   :sunny, :rainy, :stormy, :snowy, :partly\_cloudy, :cloudy, :hailing, :heavy\_seas, :calm\_seas, :foggy, :snow\_flurries, :windy, :clear\_moon, :partly\_cloudy
     attr_accessor :weather
+
+    def initialize
+      @weather = Hash.new
+    end
+
+    # This method validates the metag for the submission
+    # @return Boolean
+    def valid?
+      return false if @weather[:status].nil?
+      
+      
+    end
+        
   end
 end
