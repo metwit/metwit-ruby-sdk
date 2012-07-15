bdb009d256245dad1c293ed19bd81a7d933cd2cd
Simone D'Amico <sim@me.com>
2012-07-15 17:51:45 +0200

    escaping _ in markdown

--- lib/metwit/metag.rb
+++ lib/metwit/metag.rb
@@ -5,7 +5,7 @@ module Metwit
     #   Is always required in a valid Metag.  
     #   Weather is an Hash with two keys: :status and :details
     #   Valid :status values are:  
-    #   :sunny, :rainy, :stormy, :snowy, :partly_cloudy, :cloudy, :hailing, :heavy_seas, :calm_seas, :foggy, :snow_flurries, :windy, :clear_moon, :partly_cloudy
+    #   :sunny, :rainy, :stormy, :snowy, :partly\_cloudy, :cloudy, :hailing, :heavy\_seas, :calm\_seas, :foggy, :snow\_flurries, :windy, :clear\_moon, :partly\_cloudy
     attr_accessor :weather
   end
 end
