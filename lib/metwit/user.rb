74b66963104c12419cbf951197c911e7a80fe164
Simone D'Amico <sim@me.com>
2012-07-17 15:35:06 +0200

    added User::find

--- lib/metwit/user.rb
+++ lib/metwit/user.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 module Metwit
 
   # A user of Metwit
@@ -28,10 +30,11 @@ module Metwit
     # @return [URI] avatar url
     attr_accessor :avatar
 
+    # Guaranteed.
     # Tells if you follow the user
     # @return [Boolean]
     def followed?
-      @followed
+      @is_followed
     end
 
     # Guaranteed.
@@ -52,21 +55,58 @@ module Metwit
     # @return [Array<Symbol>] Bagdges
     attr_accessor :badges
 
+    # Guaranteed.
     # Tells if the user is connected with facebook
     # @return [Boolean]
     def facebook?
       @has_facebook
     end
-    
+
+    # Guaranteed.
     # Tells if the user is connected with twitter
     def twitter?
       @has_twitter
     end
 
-    # Private
+    # Personal
     # The user email
     # @return [String]
     attr_accessor :email
+
+    def initialize(options={})
+      @id = options[:id]
+      @nickname = options[:nickname]
+      @metags_count = options[:metags_count]
+      @today_metags_count = options[:today_metags_count]
+      @avatar = options[:avatar]
+      @is_followed = options[:followed]
+      @followers_count = options[:followers_count]
+      @following_count = options[:following_count]
+      @has_facebook = options[:has_facebook]
+      @has_twitter = options[:has_twitter]
+    end
     
+    class << self
+      # Return the user associate with the id
+      # @return [User]
+      def find(id)
+        options = {
+          id: 3,
+          nickname: 'verginge',
+          metags_count: 100,
+          today_metags_count: 2,
+          avatar: URI('https://s3.salcazzo.amazon.com/di233hane'),
+          is_followed: false,
+          followers_count: 10,
+          following_count: 100,
+          has_facebook: true,
+          has_twitter: false,
+        }
+
+        return User.new(options)
+      end
+      
+    end
+      
   end
 end
--- spec/metwit/user_spec.rb
+++ spec/metwit/user_spec.rb
@@ -2,7 +2,47 @@ require 'spec_helper'
 module Metwit
   describe User do
     def user
-      @user ||= User.new
+      options = {
+        id: 3,
+        nickname: 'verginge',
+        metags_count: 100,
+        today_metags_count: 2,
+        avatar: URI('https://s3.salcazzo.amazon.com/di233hane'),
+        is_followed: false,
+        followers_count: 10,
+        following_count: 100,
+        has_facebook: true,
+        has_twitter: false,
+      }
+      @user ||= User.new(options)
+    end
+
+    describe "::find" do
+      
+      it "should return a User object" do
+        User.find(3).should be_a User
+      end
+
+      it "should return a User with the id searched" do
+        id = 3
+        user = User.find(3)
+        user.id.should eq id
+      end
+      
+      it "should have all the guaranteed fields" do
+        user = User.find(3)
+        user.id.should_not be_nil
+        user.nickname.should_not be_nil
+        user.metags_count.should_not be_nil
+        user.today_metags_count.should_not be_nil
+        user.avatar.should_not be_nil
+        user.followed?.should_not
+        user.followers_count.should_not be_nil
+        user.following_count.should_not be_nil
+        user.facebook?
+        user.twitter?
+      end
+      
     end
     
   end
